abf91558034f83d9c75242315fc04735a6d81860
Rushik-B <rushikbusiness@gmail.com>
2025-02-13 14:57:22 -0800

    Changed the projects section and reduced the number of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,35 +11,21 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Placeholder",
+      description: "Loreum Ipseum.....",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Placeholder",
+      description: "Loreum Ipseum.....",
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Placeholder",
+      description: "Loreum Ipseum.....",
       imgUrl: projImg3,
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    
   ];
 
   return (
